9b3d0dbb7dd935284aff96874f5e9e43c2b289cb
hakanyalniz <hakan_yalniz@live.com>
2025-02-28 17:27:58 +0300

    Added some basic CSS to the navbar, seperated them into two sides of navbar. Added some button effects on the text.

--- src/components/NavBar/TopNavBar.tsx
+++ src/components/NavBar/TopNavBar.tsx
@@ -3,8 +3,13 @@ import "./TopNavBar.css";
 const TopNavBar = () => {
   return (
     <div className="top-nav">
-      <span>Home</span>
-      <span>Instructions</span>
+      <div className="left-nav">
+        <span className="nav-button">Home</span>
+        <span className="nav-button">Instructions</span>
+      </div>
+      <div className="right-nav">
+        <span className="nav-button">Add Ebook</span>
+      </div>
     </div>
   );
 };

d47ad527a289f146e3d4c1daa4b1679fab8c8130
hakanyalniz <hakan_yalniz@live.com>
2025-03-18 18:30:49 +0300

    Added some basic pagination abilities to dictionary page.

--- src/pages/Dictionary/Dictionary.tsx
+++ src/pages/Dictionary/Dictionary.tsx
@@ -5,7 +5,19 @@ import { useEffect, useState } from "react";
 
 function Dictionary() {
   const [searchResults, setSearchResults] = useState([]);
+  const [currentPagination, setCurrentPagination] = useState(0);
   let searchBar: HTMLInputElement;
+
+  // Change the below values to play with pagination numbers
+  const ITEM_PER_PAGINATION = 13;
+
+  // So, currentPageFirstItem will be 0, 10, 20, 30 and so on
+  // currentPageLastItem will be 10, 20, 30, 40 and so on
+  // the logic here is that lastItem will be 10 ahead of first item
+  // This way, we get 10 slices off the array in order
+  const currentPageFirstItem = currentPagination * ITEM_PER_PAGINATION;
+  const currentPageLastItem =
+    currentPagination * ITEM_PER_PAGINATION + ITEM_PER_PAGINATION;
 
   const handleFormButtonClick = (event: { preventDefault: () => void }) => {
     event.preventDefault();
@@ -18,7 +30,11 @@ function Dictionary() {
 
     handleDataFetching({
       query: searchBar.value,
-    }).then((data) => setSearchResults(data.slice(0, 20)));
+    }).then((data) => {
+      console.log(data);
+
+      return setSearchResults(data);
+    });
   };
 
   useEffect(() => {
@@ -26,6 +42,25 @@ function Dictionary() {
     searchBar = document.getElementById("searchBar") as HTMLInputElement;
     searchBar.addEventListener("input", handleFormButtonClick);
   }, []);
+
+  const handleNextPagination = () => {
+    setCurrentPagination((prevState) => {
+      return (prevState = prevState + 1);
+    });
+  };
+
+  const handlePrevPagination = () => {
+    setCurrentPagination((prevState) => {
+      if (prevState > 0) {
+        return (prevState = prevState - 1);
+      }
+      return prevState;
+    });
+  };
+
+  useEffect(() => {
+    console.log(currentPagination);
+  });
 
   return (
     <div>
@@ -46,17 +81,24 @@ function Dictionary() {
             </tr>
           </thead>
           <tbody>
-            {searchResults.map((item, index) => {
-              return (
-                <tr key={index}>
-                  <td> {item["kanji"]}</td>
-                  <td> {item["kana"]}</td>
-                  <td> {item["meaning"]}</td>
-                </tr>
-              );
-            })}
+            {searchResults
+              .slice(currentPageFirstItem, currentPageLastItem)
+              .map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td> {item["kanji"]}</td>
+                    <td> {item["kana"]}</td>
+                    <td> {item["meaning"]}</td>
+                  </tr>
+                );
+              })}
           </tbody>
         </table>
+      </div>
+      <div className="pagination-buttons">
+        <button onClick={handlePrevPagination}>Prev</button>
+        <span className="pagination-number">{currentPagination}</span>
+        <button onClick={handleNextPagination}>Next</button>
       </div>
     </div>
   );
